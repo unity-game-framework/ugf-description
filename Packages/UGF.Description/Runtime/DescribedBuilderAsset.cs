bf89cee2cddbcc6ddfaa197f80fdbf072ddfdcd5
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-30 23:42:18 +0300

    Update DescribedBuilderAsset.cs

--- Packages/UGF.Description/Runtime/DescribedBuilderAsset.cs
+++ Packages/UGF.Description/Runtime/DescribedBuilderAsset.cs
@@ -4,6 +4,11 @@ namespace UGF.Description.Runtime
 {
     public abstract class DescribedBuilderAsset<TDescribed> : BuilderAsset<TDescribed>, IDescribedBuilder where TDescribed : class, IDescribed
     {
+        T IBuilder<IDescribed>.Build<T>()
+        {
+            return (T)(object)OnBuild();
+        }
+
         IDescribed IBuilder<IDescribed>.Build()
         {
             return OnBuild();
